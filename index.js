1746c6c576fc2dfbe47e3c5d9f552d936f2b5a84
Michelle Glinkowski <miglinkowski@aol.com>
2023-04-02 16:39:17 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+{let num1 = 2;
+let num2 = 31;
+let multiply = num1*num2}
 
-// Write your code here
+{let random = 10
+Math.floor(Math.random() * 10) + 1;}
+
+{let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4}
+
+let max = (5, 10, 15, 20);
